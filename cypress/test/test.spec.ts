5c62201f00aad1a00d5b6977ff8f7c7fb94f4c41
weibin chiu <chiuweibin.dev@gmail.com>
2022-04-04 01:31:33 +0800

    first commit

---
+++ cypress/test/test.spec.ts
@@ -0,0 +1,48 @@
+/// <reference types="cypress" />
+/// <reference types="@shelex/cypress-allure-plugin" />
+
+describe('Parabank - demo site', () => {
+  beforeEach(() => {
+    cy.allure().logStep('Open the application')
+
+    cy.visit('/parabank/index.htm')
+    cy.allure()
+      .severity('blocker')
+      .description('Login page')
+      .owner('Chiu Wei Bin')
+      .story('Register a new user')
+      .tms('XRay', '123')
+      .tag('this is nice tag', 'as well as this')
+  })
+
+  it('contains correct url', () => {
+    cy.allure().writeEnvironmentInfo({
+      Browser: Cypress.browser.displayName,
+      Version: Cypress.browser.version,
+      Channel: Cypress.browser.channel,
+      Family: Cypress.browser.family,
+    })
+    cy.allure().logStep('Check the correct url')
+
+    cy.url().should(
+      'equal',
+      Cypress.config().baseUrl + '/parabank/index.htm111',
+    )
+  })
+
+  it('redirect to sign up', () => {
+    cy.allure().logStep('Click on the register button')
+
+    cy.contains('#loginPanel  p', 'Register').find('a').click()
+    cy.allure().logStep('Check the register URL')
+
+    cy.url().should(
+      'include',
+      Cypress.config().baseUrl + '/parabank/register.htm;jsessionid=',
+    )
+    // cy.url().contains(
+    //   Cypress.config().baseUrl + '/parabank/register.htm;jsessionid=',
+    // )
+    // TODOadd regex
+  })
+})
---
+++ report.bat
@@ -0,0 +1,1 @@
+npm run allure:report && allure open
